9f962726d72ba36a36f25652bc60e0edcd8de6ad
Deepak Kharah <kharahdeepak009@gmail.com>
2020-08-02 10:53:14 +0530

    templates

--- src/attendance/validators.py
+++ src/attendance/validators.py
@@ -1,18 +1,15 @@
 from django.core.exceptions import ValidationError
 from django.forms import ValidationError as FormValidationError
 from django.utils.timezone import now as today
-from django.utils.translation import gettext as _
 
-error_messages = {
-    'future_date_error': {'message': _('Date cannot be from future'), 'code': 'date_future_error'}
-}
+from .utils import ERROR_MESSAGES
 
 
 def validate_no_future_date(date):
     if date > today().date():
-        raise ValidationError(**error_messages.get('future_date_error'))
+        raise ValidationError(**ERROR_MESSAGES.get('future_date_error'))
 
 
 def validate_no_future_date_form(date):
     if date > today().date():
-        raise FormValidationError(**error_messages.get('future_date_error'))
+        raise FormValidationError(**ERROR_MESSAGES.get('future_date_error'))
