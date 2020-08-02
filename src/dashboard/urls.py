5e9e10a7d0c2747cf88892459d016ce53a7cfe1f
Nitu <nitus22499@gmail.com>
2020-08-02 21:08:39 +0530

    school dashboard

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 
-from .views import DashboardView
+from .views import DashboardView, chart, get_blocks_by_district
 
 
 urlpatterns = [
     path('school_dashboard/', DashboardView.as_view(), name='school_dashboard'),
+    path('chart/', chart, name='chart'),
+    path('get-blocks/', get_blocks_by_district, name='get_blocks')
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
+from django.db.models import Count, Q
+from django.http import JsonResponse
 
-from misc.utilities import academic_year
+from misc.utilities import academic_year, year_choices, get_districts, get_blocks
 
 from schoolinfo.models import PhysicalFacilities
 
@@ -17,20 +19,47 @@ class DashboardView(generic.TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        
         if 'year' in self.request.GET:
-            year = self.request.GET['year']
+            if self.request.GET['year'] != "":
+                year = Q(academic_year = self.request.GET['year'])
+                context['year'] = self.request.GET['year']
+            else: 
+                year = Q()
         else:
-            year = academic_year()
+            year = Q()
 
-        context['no_of_school'] = PhysicalFacilities.objects.filter(academic_year=year).count()
-        context['no_of_government_schools'] = PhysicalFacilities.objects.filter(pf_status='Government').filter(academic_year=year).count()
-        context['no_of_school_with_drinking_water'] = PhysicalFacilities.objects.filter(pf_drinking_water_available='Yes').filter(academic_year=year).count()
-        context['no_of_school_have_playground_facility'] = PhysicalFacilities.objects.filter(pf_playground_facility='Yes').filter(academic_year=year).count()
-        context['no_of_school_with_electricity'] = PhysicalFacilities.objects.filter(pf_electricity_connection='Yes').filter(academic_year=year).count()
-        context['no_of_school_with_library'] = PhysicalFacilities.objects.filter(pf_librarian='Yes').filter(academic_year=year).count()
-        context['no_of_school_with_seperate_girl_common_room'] = PhysicalFacilities.objects.filter(pf_sep_girls_sec='Yes').filter(academic_year=year).count()
-        context['no_of_school_with_ramp'] = PhysicalFacilities.objects.filter(pf_ramp_disabled='Yes').filter(academic_year=year).count()
-        context['no_of_school_with_toilet'] = PhysicalFacilities.objects.filter(pf_schools_toilet='Yes').filter(academic_year=year).count()
+        if 'district' in self.request.GET:
+            if self.request.GET['district'] != "":
+                district = Q(pf_school__school_district = int(self.request.GET['district']))
+                context['district_id'] = self.request.GET['district']
+            else: 
+                district = Q()
+        else:
+            district = Q()
+
+        if 'block' in self.request.GET:
+            if self.request.GET['block'] != "":
+                block = Q(pf_school__school_block = int(self.request.GET['block']))
+                context['block_id'] = self.request.GET['block']
+            else: 
+                block = Q()
+        else:
+            block = Q()
+
+        context['years'] = year_choices()
+        context['blocks'] = get_blocks()
+        context['districts'] = get_districts()
+        
+        context['no_of_school'] = PhysicalFacilities.objects.filter(year).filter(district).filter(block).count()
+        context['no_of_government_schools'] = PhysicalFacilities.objects.filter(pf_status='Government').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_drinking_water'] = PhysicalFacilities.objects.filter(pf_drinking_water_available='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_have_playground_facility'] = PhysicalFacilities.objects.filter(pf_playground_facility='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_electricity'] = PhysicalFacilities.objects.filter(pf_electricity_connection='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_library'] = PhysicalFacilities.objects.filter(pf_librarian='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_seperate_girl_common_room'] = PhysicalFacilities.objects.filter(pf_sep_girls_sec='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_ramp'] = PhysicalFacilities.objects.filter(pf_ramp_disabled='Yes').filter(year).filter(district).filter(block).count()
+        context['no_of_school_with_toilet'] = PhysicalFacilities.objects.filter(pf_schools_toilet='Yes').filter(year).filter(district).filter(block).count()
 
         context['no_of_government_schools'] = get_percentage(context['no_of_government_schools'], context['no_of_school'])
         context['no_of_school_with_drinking_water'] = get_percentage(context['no_of_school_with_drinking_water'], context['no_of_school'])
@@ -41,3 +70,40 @@ class DashboardView(generic.TemplateView):
         context['no_of_school_with_ramp'] = get_percentage(context['no_of_school_with_ramp'], context['no_of_school'])
         context['no_of_school_with_toilet'] = get_percentage(context['no_of_school_with_toilet'], context['no_of_school'])
         return context
+
+
+def chart(request, *args, **kwargs):
+    label = []
+    data = []
+    query = {}
+    
+    if 'year' in request.GET and request.GET['year'] != '':
+        query['academic_year'] = request.GET['year']
+    if 'district' in request.GET and request.GET['district'] != '':
+        query['pf_school__school_district'] = int(request.GET['district'])
+    if 'block' in request.GET and request.GET['block'] != '':
+        query['pf_school__school_block'] = int(request.GET['block'])
+    if 'field' in request.GET and request.GET['value'] != '' and request.GET['field'] != '':
+        query[request.GET['field']] = request.GET['value']
+    
+    values = PhysicalFacilities.objects.filter(**query).values_list('academic_year').annotate(freq=Count('academic_year'))
+    for value in values:
+        label.append(value[0])
+        data.append(value[1])
+    return JsonResponse(data = {
+        'label': label,
+        'data' : data
+    })
+    
+
+def get_blocks_by_district(request):
+    blocks = None
+    if 'district_id' in request.GET:
+        if request.GET['district_id'] == "":
+            blocks = get_blocks()
+        else:
+            district_id = int(request.GET['district_id'])
+            blocks = get_blocks(district_id)
+    return JsonResponse(data = {
+        'blocks' : blocks
+    })
